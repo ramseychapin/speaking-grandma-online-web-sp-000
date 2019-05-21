909341daed8e2c59e09eff0d149dd5ac0f5b1db9
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-21 03:54:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,6 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 
-def speak_to_grandma
-
+def speak_to_grandma (statement)
+  if (statement == statement.upcase)
+    "NO, NOT SINCE 1938!"
+  elsif (statement == "I LOVE YOU GRANDMA!")
+    "I LOVE YOU TOO PUMPKIN!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
 end
